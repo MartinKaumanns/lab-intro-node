ba157d0cf3c148ef24d1fb5d65e2e1375d3b8120
Martin <martin.kaumanns@diebrueder.com>
2022-03-24 14:41:36 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos) === -1) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items.indexOf(pos);
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      // return Math.max(...this.items);
+      return this.items.pop();
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      // return Math.min(...this.items);
+      return this.items.shift();
+    }
+  }
 
-  sum() {}
+  sum() {
+    const sumOfItems = this.items.reduce((accu, item) => {
+      return accu + item;
+    }, 0);
+    return sumOfItems;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      const avgOfItems = this.items.reduce((accu, item) => {
+        return accu + item;
+      }, 0);
+      return avgOfItems / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
